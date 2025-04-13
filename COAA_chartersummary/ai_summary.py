11325956c3435ec073d1d93b5b5101459d8fe21d
bger-dhoward <dhoward@ballinger.com>
2025-04-13 14:49:24 -0400

    test adding Jinja2 template and outputting to HTML

--- COAA_chartersummary/ai_summary.py
+++ COAA_chartersummary/ai_summary.py
@@ -1,8 +1,13 @@
 import ollama
 import os
 import csv
+from jinja2 import Environment, FileSystemLoader
 
-os.chdir(r'C:\Users\howar\OneDrive - ballingercloud\Documents\aisummary')
+environment = Environment(loader=FileSystemLoader("templates/"))
+template = environment.get_template('page_template.txt')
+
+
+# os.chdir(r'C:\Users\howar\OneDrive - ballingercloud\Documents\aisummary')
 
 def get_user_responses(filename):
     with open(filename, encoding='utf-8-sig') as file:
@@ -15,7 +20,7 @@ def generate_prompt(data):
     user_responses = [r[0] for r in data[1:]]
     #print(user_responses)
     user_resp_string = "\n".join(user_responses)
-    prompt = f"A group of users in a kickoff meeting for VT university School of Medicine new building project were asked to provide responses for a prompt of '{category}'. Their responses will be at the end of this message.  Provide about 5 concise single short sentence summaries that synthesize these responses into a set of potential guiding principles for the design of the project. Prefix each summary sentence with '%'. These are their responses:\n{user_resp_string}"
+    prompt = f"A group of users in a kickoff meeting for VT university School of Medicine new building project were asked to provide responses for a prompt of '{category}'. Their responses will be at the end of this message.  Provide about 5 concise single short sentence summaries that synthesize these responses into a set of potential guiding principles for the design of the project. Prefix each summary sentence with '%'. Do not include any introductory text or line number. These are their responses:\n{user_resp_string}"
     return prompt
 
 def ask_model(prompt):
@@ -27,15 +32,27 @@ def ask_model(prompt):
 
 
 if __name__ == "__main__":
+    print(os.getcwd())
+    input('continue?')
     for filename in [fn for fn in os.listdir() if fn.endswith('.csv')]:
         data = get_user_responses(filename)
+        category = data[0][0]
         # print(data)
         prompt = generate_prompt(data)
         print(filename, data[0])
         response = ask_model(prompt)
-        print(response)
-        print('\n\n')
-        input('next?\n\n')
+        summary = response.split("\n")
+
+        context = {"category": category,
+                   "summary": summary}
+        #print(response)
+        #print('\n\n')
+        #input('next?\n\n')
+
+        results_filename = category + ".html"
+        with open(results_filename, 'w', encoding='utf-8') as results:
+            results.write(template.render(context))
+
     
     input('hit enter to exit')
 
